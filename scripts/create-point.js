8c47c2792d6f7c8147ad1ce6043ec059e1b7eec3
Felipe Morais Mariano dos Santos <tfelipesantos99s@gmail.com>
2020-06-04 20:24:55 -0300

    Finishi style , interaction and form

--- scripts/create-point.js
+++ scripts/create-point.js
@@ -5,8 +5,8 @@ function populateUFs() {
   const ufSelect = document.querySelector('select[name=uf]');
 
   fetch('https://servicodados.ibge.gov.br/api/v1/localidades/estados')
-    .then(res => res.json())
-    .then(states => {
+    .then((res) => res.json())
+    .then((states) => {
       for (const state of states) {
         ufSelect.innerHTML += `<option value="${state.id}">${state.nome}</option>`;
       }
@@ -14,29 +14,71 @@ function populateUFs() {
 }
 
 function getcities(event) {
-    const citySelect = document.querySelector('select[name=city]');
-    const stateInput = document.querySelector('input[name=state]');
-    
-    const ufValue = event.target.value
+  const citySelect = document.querySelector('select[name=city]');
+  const stateInput = document.querySelector('input[name=state]');
 
-    const indexOfSelectdState = event.target.selectedIndex
+  const ufValue = event.target.value;
 
-    stateInput.value = event.target.options[indexOfSelectdState].text
+  const indexOfSelectdState = event.target.selectedIndex;
 
-    const url = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufValue}/municipios
-    `
+  stateInput.value = event.target.options[indexOfSelectdState].text;
 
-    fetch(url)
-    .then(res => res.json())
-    .then(cities => {
+  const url = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufValue}/municipios
+    `;
+  citySelect.innerHTML = '<option value>Selecione a cidade</optioni>';
+
+  citySelect.disabled = true;
+
+  fetch(url)
+    .then((res) => res.json())
+    .then((cities) => {
       for (const city of cities) {
-        citySelect.innerHTML += `<option value="${city.id}">${city.nome}</option>`;
+        citySelect.innerHTML += `<option value="${city.nome}">${city.nome}</option>`;
       }
-      citySelect.disabled = false
+
+      citySelect.disabled = false;
     });
-    
-  }
+}
 
 populateUFs();
 
 document.querySelector('select[name=uf]').addEventListener('change', getcities);
+
+// Items de coleta
+
+const itemsToCollect = document.querySelectorAll('.items-grid li');
+
+for (const item of itemsToCollect) {
+  item.addEventListener('click', handleSelectedItem);
+}
+
+const collectedItems = document.querySelector('input[name=items]');
+
+
+let selectedItems = []
+
+function handleSelectedItem(event) {
+  const itemLi = event.target;
+
+  // add or remove class
+  itemLi.classList.toggle('selected');
+
+  const itemId = itemLi.dataset.id;
+
+  const alreadySelected = selectedItems.findIndex(item => item  == itemId)
+
+  if (alreadySelected != -1) {
+    const filteredItems = selectedItems.filter(item => item != itemId)
+
+    selectedItems = filteredItems     
+
+  } else {
+    selectedItems.push(itemId)
+  }
+
+  collectedItems.value = selectedItems
+  
+}
+
+
+
