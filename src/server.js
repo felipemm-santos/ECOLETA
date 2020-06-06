daceb8e9c3d0a016329dba6eb8d87ef41325bdab
Felipe Morais Mariano dos Santos <tfelipesantos99s@gmail.com>
2020-06-05 23:10:08 -0300

    Add nodemon package to dev dependencies

--- src/server.js
+++ src/server.js
@@ -1,6 +1,14 @@
 const express = require("express")
 const server  = express()
 
+//Configurar caminhos da aplicação
+// pagina inicial
+// req: requisição
+// res: resposta
+server.get("/", (req, res) => {
+    res.send("Cheguei aqui")
+})
+
 // ligar o servidor
 
 server.listen(3000)
