7c77e2514327a3076cf4110bd0e5ed9d9b03fd36
Felipe Morais Mariano dos Santos <tfelipesantos99s@gmail.com>
2020-06-07 16:58:14 -0300

    Setting aplication to use database

--- src/database/db.js
+++ src/database/db.js
@@ -4,72 +4,69 @@ const sqlite3 = require('sqlite3').verbose();
 // Iniciar o objeto que irá fazer operações no  banco de dados
 const db = new sqlite3.Database('./src/database/database.db');
 
-// utilizar o objeto de banco de dados
+// Exportar arquivo para a aplicação
+module.exports = db;
+
+// // Utilizar o objeto de banco de dados
 // db.serialize(() => {
-//   // Cria uma tabela
-//   db.run(`
-//     CREATE TABLE IF NOT EXISTS places (
-//         id INTEGER PRIMARY KEY AUTOINCREMENT,
-//         image TEXT,
-//         name TEXT,
-//         address TEXT,
-//         address2 TEXT,
-//         state TEXT,
-//         city TEXT,
-//         items TEXT
-//     );
-// `);
-
-//   //Inserir dados na tabelas
-//   const query = `
-//     INSERT INTO places (
-//         image,
-//         name,
-//         address,
-//         address2,
-//         state,
-//         city,
-//         items
-//     ) VALUES (?,?,?,?,?,?,?);
-//     `;
-
-//   const values = [
-//     'https://images.unsplash.com/photo-1567393528677-d6adae7d4a0a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80',
-//     'Papersider',
-//     'Guilherme Gemballa, Jardim América',
-//     'Número 260',
-//     'Santa Catarina',
-//     'Rio do Sul',
-//     'Resíduos eletrônicos, Lâmpadas',
-//   ];
-
-//   function afterInsertData(err) {
-//     if (err) {
-//       return console.log(err);
-//     }
-//     console.log('Cadastrado com sucesso');
-//     console.log(this);
-//   }
-
-//   db.run(query, values, afterInsertData);
-
-//   //Consultar dados na tabela
-// //   db.all(`SELECT name FROM places`, function(err, rows){
-// //     if (err) {
-// //         return console.log(err);
-// //       }
-// //       console.log("Aqui estão seus registros");
-// //       console.log(rows);    
-      
-// //   })
-  
-  
-//   //Deletar um dado na tabela
-// //   db.run(`DELETE FROM places WHERE id = ?` , [1], function(err){
-// //     if (err) {
-// //         return console.log(err);
-// //       }
-// //       console.log("Registro deletado com sucesso");      
-// //   })
-
+//   //   // Cria uma tabela
+//   //   db.run(`
+//   //     CREATE TABLE IF NOT EXISTS places (
+//   //         id INTEGER PRIMARY KEY AUTOINCREMENT,
+//   //         image TEXT,
+//   //         name TEXT,
+//   //         address TEXT,
+//   //         address2 TEXT,
+//   //         state TEXT,
+//   //         city TEXT,
+//   //         items TEXT
+//   //     );
+//   // `);
+//   //
+//   //   //Inserir dados na tabela
+//   //   const query = `
+//   //     INSERT INTO places (
+//   //         image,
+//   //         name,
+//   //         address,
+//   //         address2,
+//   //         state,
+//   //         city,
+//   //         items
+//   //     ) VALUES (?,?,?,?,?,?,?);
+//   //     `;
+//   //   const values = [
+//   //     'https://images.unsplash.com/photo-1567393528677-d6adae7d4a0a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80',
+//   //     'Papersider',
+//   //     'Guilherme Gemballa, Jardim América',
+//   //     'Número 260',
+//   //     'Santa Catarina',
+//   //     'Rio do Sul',
+//   //     'Resíduos eletrônicos, Lâmpadas',
+//   //   ];
+//   //   function afterInsertData(err) {
+//   //     if (err) {
+//   //       return console.log(err);
+//   //     }
+//   //     console.log('Cadastrado com sucesso');
+//   //     console.log(this);
+//   //   }
+//   //
+//   //   db.run(query, values, afterInsertData);
+//   //
+//   // // Deletar um dado na tabela
+//   // db.run(`DELETE FROM places WHERE id = ?`, [7], function (err) {
+//   //   if (err) {
+//   //     return console.log(err);
+//   //   }
+//   //   console.log('Registro deletado com sucesso');
+//   // });
+//   // //Consultar dados na tabela
+//   // db.all(`SELECT * FROM places`, function (err, rows) {
+//   //   if (err) {
+//   //     return console.log(err);
+//   //   }
+//   //   console.log('Aqui estão seus registros');
+//   //   console.log(rows);
+//   // });
 // });
--- src/server.js
+++ src/server.js
@@ -1,31 +1,94 @@
-const express = require("express")
-const server  = express()
+const express = require('express');
+const server = express();
+
+// pegar o banco de dados
+const db = require('./database/db.js');
 
 // Utilizando template engine
-const nunjucks = require("nunjucks")
-nunjucks.configure("src/views", {
-    express: server,
-    noCache: true
-})
+const nunjucks = require('nunjucks');
+nunjucks.configure('src/views', {
+  express: server,
+  noCache: true,
+});
 
 //Configurar pasta publica
-server.use(express.static("public"))
+server.use(express.static('public'));
+
+// Habilitar re.body na aplicação
+server.use(express.urlencoded({ extended: true }));
 
 //Configurar caminhos da aplicação
 // pagina inicial
 // req: requisição
 // res: resposta
-server.get("/", (req, res) => {
-   return res.render("index.html")
-})
-server.get("/create-point", (req, res) => {
-   return res.render("create-point.html")
-})
-server.get("/search", (req, res) => {
-   return res.render("search-results.html")
-})
+server.get('/', (req, res) => {
+  return res.render('index.html');
+});
 
+server.get('/create-point', (req, res) => {
+  // Query strings da url
+  // console.log(req.query);
+
+  return res.render('create-point.html');
+});
+
+server.post('/save-point', (req, res) => {
+  // req.body: corpo do nosso formulário
+  // console.log(req.body);
+
+  // Inserir dados na tabela
+  const query = `
+      INSERT INTO places (
+          name,
+          image,
+          address,
+          address2,
+          state,
+          city,
+          items
+      ) VALUES (?,?,?,?,?,?,?);
+      `;
+
+  const values = [
+    req.body.name,
+    req.body.image,
+    req.body.address,
+    req.body.address2,
+    req.body.state,
+    req.body.city,
+    req.body.items,
+  ];
+
+  function afterInsertData(err) {
+    if (err) {
+      return console.log(err);
+    }
+
+    console.log('Cadastrado com sucesso');
+    console.log(this);
+
+    return res.send('Ok');
+  }
+
+  db.run(query, values, afterInsertData);
+});
+
+server.get('/search', (req, res) => {
+  // pegar os dados do banco de dados
+  db.all(`SELECT * FROM places`, function (err, rows) {
+    if (err) {
+      return console.log(err);
+    }
+    //  console.log('Aqui estão seus registros');
+    //  console.log(rows);
+
+    const total = rows.length;
+
+    //Mostra a página html com os dados do banco de dados
+    return res.render('search-results.html', { places: rows, total });
+  });
+});
 
 // ligar o servidor
 
-server.listen(3000)
+server.listen(3000);
