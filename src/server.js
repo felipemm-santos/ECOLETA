e4d0106c89702d93a3af6ed9c0bfc5aabf08613d
Felipe Morais Mariano dos Santos <tfelipesantos99s@gmail.com>
2020-06-06 22:12:25 -0300

    configure nunjuks

--- src/server.js
+++ src/server.js
@@ -1,13 +1,30 @@
 const express = require("express")
 const server  = express()
 
+// Utilizando template engine
+const nunjucks = require("nunjucks")
+nunjucks.configure("src/views", {
+    express: server,
+    noCache: true
+})
+
+//Configurar pasta publica
+server.use(express.static("public"))
+
 //Configurar caminhos da aplicação
 // pagina inicial
 // req: requisição
 // res: resposta
 server.get("/", (req, res) => {
-    res.send("Cheguei aqui")
+   return res.render("index.html")
 })
+server.get("/create-point", (req, res) => {
+   return res.render("create-point.html")
+})
+server.get("/page-search-results", (req, res) => {
+   return res.render("page-search-results.html")
+})
+
 
 // ligar o servidor
 
