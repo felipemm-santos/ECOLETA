fe5db0cabe6fef5a26a277545414bc5b7071a4c8
Felipe Morais Mariano dos Santos <tfelipesantos99s@gmail.com>
2020-06-07 13:14:52 -0300

    Manually delete data from table

--- src/database/db.js
+++ src/database/db.js
@@ -62,3 +62,14 @@ const db = new sqlite3.Database('./src/database/database.db');
 // //       console.log(rows);    
       
 // //   })
+  
+  
+//   //Deletar um dado na tabela
+// //   db.run(`DELETE FROM places WHERE id = ?` , [1], function(err){
+// //     if (err) {
+// //         return console.log(err);
+// //       }
+// //       console.log("Registro deletado com sucesso");      
+// //   })
+
+// });
