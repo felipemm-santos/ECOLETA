b0d805babb607453da27822a22067620b976aa93
Felipe Morais Mariano dos Santos <tfelipesantos99s@gmail.com>
2020-06-07 12:17:08 -0300

    Create database with sqlite3

--- src/database/db.js
+++ src/database/db.js
@@ -1,2 +1,5 @@
 // Importar a dependência do sqlite3
 const sqlite3 = require("sqlite3").verbose()
+
+// Iniciar o objeto que irá fazer operações no  banco de dados
+const db = new sqlite3.Database("./src/database/database.db")
