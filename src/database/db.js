5dc55a6e77ea3ac4e48d755e7555585e61b060c0
Felipe Morais Mariano dos Santos <tfelipesantos99s@gmail.com>
2020-06-07 13:14:05 -0300

    Insert data in table manually

--- src/database/db.js
+++ src/database/db.js
@@ -19,3 +19,37 @@ const db = new sqlite3.Database('./src/database/database.db');
 //         items TEXT
 //     );
 // `);
+
+//   //Inserir dados na tabelas
+//   const query = `
+//     INSERT INTO places (
+//         image,
+//         name,
+//         address,
+//         address2,
+//         state,
+//         city,
+//         items
+//     ) VALUES (?,?,?,?,?,?,?);
+//     `;
+
+//   const values = [
+//     'https://images.unsplash.com/photo-1567393528677-d6adae7d4a0a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80',
+//     'Papersider',
+//     'Guilherme Gemballa, Jardim América',
+//     'Número 260',
+//     'Santa Catarina',
+//     'Rio do Sul',
+//     'Resíduos eletrônicos, Lâmpadas',
+//   ];
+
+//   function afterInsertData(err) {
+//     if (err) {
+//       return console.log(err);
+//     }
+//     console.log('Cadastrado com sucesso');
+//     console.log(this);
+//   }
+
+//   db.run(query, values, afterInsertData);
+
