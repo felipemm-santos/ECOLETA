a35f80272ad7c5f432afa506ef821452db8c7d25
Felipe Morais Mariano dos Santos <tfelipesantos99s@gmail.com>
2020-06-07 13:13:18 -0300

    Create database table
    Create the table manually

--- src/database/db.js
+++ src/database/db.js
@@ -1,5 +1,21 @@
 // Importar a dependência do sqlite3
-const sqlite3 = require("sqlite3").verbose()
+const sqlite3 = require('sqlite3').verbose();
 
 // Iniciar o objeto que irá fazer operações no  banco de dados
-const db = new sqlite3.Database("./src/database/database.db")
+const db = new sqlite3.Database('./src/database/database.db');
+
+// utilizar o objeto de banco de dados
+// db.serialize(() => {
+//   // Cria uma tabela
+//   db.run(`
+//     CREATE TABLE IF NOT EXISTS places (
+//         id INTEGER PRIMARY KEY AUTOINCREMENT,
+//         image TEXT,
+//         name TEXT,
+//         address TEXT,
+//         address2 TEXT,
+//         state TEXT,
+//         city TEXT,
+//         items TEXT
+//     );
+// `);
